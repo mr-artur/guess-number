2c69c9418cda8f15e5e2f313cc7794d7310e7499
mr-artur <ar2r.pv@gmail.com>
2020-02-07 10:41:42 +0200

    1. fixed wrong input message | 2. removed extra information from pom.xml

--- src/main/java/ua/kpi/guessnumber/views/View.java
+++ src/main/java/ua/kpi/guessnumber/views/View.java
@@ -8,7 +8,7 @@ public class View {
     public static final String WELCOME_MESSAGE = "Try to guess a number! Number is between 1 and 100";
     public static final String OUT_OF_LIMITS_MESSAGE = "Number is out of limits!";
     public static final String INPUT_NUMBER = "Please, enter a number : ";
-    public static final String WRONG_INPUT = "Please, enter a number within the specified limits!";
+    public static final String WRONG_INPUT = "Please, enter an integer number within the specified limits!";
 
     public void printMessage(String message) {
         System.out.println(message);
